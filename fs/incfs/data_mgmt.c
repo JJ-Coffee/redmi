0f430ed3a8f3f71c826ab4db10d23070d2ebf2ab
JJ-Coffee <akuntrial042@gmail.com>
2025-02-05 10:49:35 +0000

    fs: incfs: fixincompatible pointer types passinggit

--- fs/incfs/data_mgmt.c
+++ fs/incfs/data_mgmt.c
@@ -572,8 +572,8 @@ static void log_block_read(struct mount_info *mi, incfs_uuid_t *id,
 	schedule_delayed_work(&log->ml_wakeup_work, msecs_to_jiffies(16));
 }
 
-static int validate_hash_tree(struct backing_file_context *bfc, struct file *f,
-			      int block_index, struct mem_range data, u8 *buf)
+static int validate_hash_tree(struct file *bf, struct file *f, int block_index,
+			      struct mem_range data, u8 *buf)
 {
 	struct data_file *df = get_incfs_data_file(f);
 	u8 stored_digest[INCFS_MAX_HASH_SIZE] = {};
@@ -630,7 +630,7 @@ static int validate_hash_tree(struct backing_file_context *bfc, struct file *f,
 		if (page)
 			put_page(page);
 
-		res = incfs_kread(bfc, buf, INCFS_DATA_FILE_BLOCK_SIZE,
+		res = incfs_kread(bf, buf, INCFS_DATA_FILE_BLOCK_SIZE,
 				  hash_block_offset[lvl] + sig->hash_offset);
 		if (res < 0)
 			return res;
@@ -1128,7 +1128,7 @@ ssize_t incfs_read_data_file_block(struct mem_range dst, struct file *f,
 	ssize_t result;
 	size_t bytes_to_read;
 	struct mount_info *mi = NULL;
-	struct backing_file_context *bfc = NULL;
+	struct file *bf = NULL;
 	struct data_file_block block = {};
 	struct data_file *df = get_incfs_data_file(f);
 
@@ -1139,7 +1139,7 @@ ssize_t incfs_read_data_file_block(struct mem_range dst, struct file *f,
 		return -ERANGE;
 
 	mi = df->df_mount_info;
-	bfc = df->df_backing_file_context;
+	bf = df->df_backing_file_context->bc_file;
 
 	result = wait_for_data_block(df, index, min_time_ms,
 			min_pending_time_ms, max_pending_time_ms, &block);
@@ -1149,21 +1149,21 @@ ssize_t incfs_read_data_file_block(struct mem_range dst, struct file *f,
 	pos = block.db_backing_file_data_offset;
 	if (block.db_comp_alg == COMPRESSION_NONE) {
 		bytes_to_read = min(dst.len, block.db_stored_size);
-		result = incfs_kread(bfc, dst.data, bytes_to_read, pos);
+		result = incfs_kread(bf, dst.data, bytes_to_read, pos);
 
 		/* Some data was read, but not enough */
 		if (result >= 0 && result != bytes_to_read)
 			result = -EIO;
 	} else {
 		bytes_to_read = min(tmp.len, block.db_stored_size);
-		result = incfs_kread(bfc, tmp.data, bytes_to_read, pos);
+		result = incfs_kread(bf, tmp.data, bytes_to_read, pos);
 		if (result == bytes_to_read) {
 			result =
 				decompress(mi, range(tmp.data, bytes_to_read),
 					   dst, block.db_comp_alg);
 			if (result < 0) {
 				const char *name =
-				    bfc->bc_file->f_path.dentry->d_name.name;
+					bf->f_path.dentry->d_name.name;
 
 				pr_warn_once("incfs: Decompression error. %s",
 					     name);
@@ -1175,7 +1175,7 @@ ssize_t incfs_read_data_file_block(struct mem_range dst, struct file *f,
 	}
 
 	if (result > 0) {
-		int err = validate_hash_tree(bfc, f, index, dst, tmp.data);
+		int err = validate_hash_tree(bf, f, index, dst, tmp.data);
 
 		if (err < 0)
 			result = err;
@@ -1256,7 +1256,7 @@ int incfs_process_new_data_block(struct data_file *df,
 
 int incfs_read_file_signature(struct data_file *df, struct mem_range dst)
 {
-	struct backing_file_context *bfc = df->df_backing_file_context;
+	struct file *bf = df->df_backing_file_context->bc_file;
 	struct incfs_df_signature *sig;
 	int read_res = 0;
 
@@ -1270,7 +1270,7 @@ int incfs_read_file_signature(struct data_file *df, struct mem_range dst)
 	if (dst.len < sig->sig_size)
 		return -E2BIG;
 
-	read_res = incfs_kread(bfc, dst.data, sig->sig_size, sig->sig_offset);
+	read_res = incfs_kread(bf, dst.data, sig->sig_size, sig->sig_offset);
 
 	if (read_res < 0)
 		return read_res;
@@ -1380,7 +1380,7 @@ static int process_file_signature_md(struct incfs_file_signature *sg,
 		goto out;
 	}
 
-	read = incfs_kread(df->df_backing_file_context, buf,
+	read = incfs_kread(df->df_backing_file_context->bc_file, buf,
 			   signature->sig_size, signature->sig_offset);
 	if (read < 0) {
 		error = read;
