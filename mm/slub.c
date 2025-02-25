571c5d21b9412975ef214d41c94c3c06375c0ebf
JJ-Coffee <akuntrial042@gmail.com>
2025-02-25 14:57:31 +0000

    update: UPSTREAM kernel-4.14.288

--- mm/slub.c
+++ mm/slub.c
@@ -2215,6 +2215,7 @@ redo:
 
 	c->page = NULL;
 	c->freelist = NULL;
+	c->tid = next_tid(c->tid);
 }
 
 /*
@@ -2350,8 +2351,6 @@ static inline void flush_slab(struct kmem_cache *s, struct kmem_cache_cpu *c)
 {
 	stat(s, CPUSLAB_FLUSH);
 	deactivate_slab(s, c->page, c->freelist, c);
-
-	c->tid = next_tid(c->tid);
 }
 
 /*
@@ -2636,6 +2635,7 @@ redo:
 
 	if (!freelist) {
 		c->page = NULL;
+		c->tid = next_tid(c->tid);
 		stat(s, DEACTIVATE_BYPASS);
 		goto new_slab;
 	}
