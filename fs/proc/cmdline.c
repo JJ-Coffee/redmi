a97298ec631f6906ffce5da7f95cb92ed82ea897
JJ-Coffee <akuntrial042@gmail.com>
2025-05-26 04:12:10 +0000

    add setup

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,6 +3,7 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
