4049becc70e9097897144f49f4325260ce21132e
JJ-Coffee <akuntrial042@gmail.com>
2025-06-30 11:35:53 +0000

    Revert "back to scope-minimized manual hooks v1.2"
    This reverts commit f34fa08a149b81bc1f9b9c0b2b84f3222ba9514b.

--- fs/exec.c
+++ fs/exec.c
@@ -1974,12 +1974,13 @@ void set_dumpable(struct mm_struct *mm, int value)
 
 #ifdef CONFIG_KSU
 extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
-			       void *__never_use_argv, void *__never_use_envp,
-			       int *__never_use_flags);
+extern __attribute__((hot)) int ksu_handle_execve_sucompat(int *fd,
+				const char __user **filename_user,
+				void *__never_use_argv, void *__never_use_envp,
+				int *__never_use_flags);
 extern int ksu_handle_execve_ksud(const char __user *filename_user,
 			const char __user *const __user *__argv);
-#ifdef CONFIG_COMPAT  // 32-on-64 support
+#ifdef CONFIG_COMPAT
 extern int ksu_handle_compat_execve_ksud(const char __user *filename_user,
 			const compat_uptr_t __user *__argv);
 #endif
@@ -1991,10 +1992,7 @@ SYSCALL_DEFINE3(execve,
 		const char __user *const __user *, envp)
 {
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execve_ksud(filename, argv);
-	else
-		ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
 #endif
 	return do_execve(getname(filename), argv, envp);
 }
